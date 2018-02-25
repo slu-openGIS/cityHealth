b7d2439dca4dda4557eae2becb97696b4126d8c1
Christopher Prener <chris.prener@slu.edu>
2018-02-25 14:23:19 -0600

    add data creation script

--- inst/extdata/createData.R
+++ inst/extdata/createData.R
@@ -1,10 +1,24 @@
 # dependencies
 library(devtools)
 library(dplyr)
+library(janitor)
 library(readr)
 
 # create full 2017 data from raw - needs to be available in the downloads
-fullData <- read_csv("/Users/chris/Downloads/500_Cities__Local_Data_for_Better_Health__2017_release.csv")
+fullData16 <- read_csv("/Users/chris/Downloads/500_Cities__Local_Data_for_Better_Health__2016_release.csv")
+
+## create us data set, 2016
+usData16 <- filter(fullData16, GeographicLevel == "US")
+attributes(usData16) <- NULL
+usData16 %>%
+  as_tibble() %>%
+  select(Year, Category, Data_Value_Type, Data_Value, Data_Value_Footnote, Low_Confidence_Limit,
+         High_Confidence_Limit, Short_Question_Text) %>%
+  clean_names() -> usData16
+use_data(usData16, overwrite = TRUE)
+
+# create full 2017 data from raw - needs to be available in the downloads
+fullData17 <- read_csv("/Users/chris/Downloads/500_Cities__Local_Data_for_Better_Health__2017_release.csv")
 
 ## create us data set, 2017
 usData17 <- filter(fullData, GeographicLevel == "US")
