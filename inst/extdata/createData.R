9bdcdfc3dd88b9f03c18ef744d95d37fe6c71c93
Christopher Prener <chris.prener@slu.edu>
2018-02-25 19:17:01 -0600

    add ch_tbl_us17

--- inst/extdata/createData.R
+++ inst/extdata/createData.R
@@ -31,10 +31,24 @@ use_data(ch_tbl_us16, overwrite = TRUE)
 fullData17 <- read_csv("/Users/chris/Downloads/500_Cities__Local_Data_for_Better_Health__2017_release.csv")
 
 ## create us data set, 2017
-usData17 <- filter(fullData, GeographicLevel == "US")
-usData17 %>%
-  as_tibble() -> usData17
-use_data(usData17, overwrite = TRUE)
+ch_tbl_us17 <- filter(fullData16, GeographicLevel == "US")
+ch_tbl_us17 %>%
+  as_tibble() %>%
+  select(Year, Category, Short_Question_Text, Data_Value_Type, Data_Value, Low_Confidence_Limit,
+         High_Confidence_Limit) %>%
+  clean_names() %>%
+  rename(
+    estimate_type = data_value_type,
+    estimate = data_value,
+    estimate_ci_low = low_confidence_limit,
+    estimate_ci_high = high_confidence_limit,
+    question = short_question_text
+  ) %>%
+  arrange(category, question, estimate_type) -> ch_tbl_us17
+
+attr(ch_tbl_us17,'spec') <- NULL
+
+use_data(ch_tbl_us17, overwrite = TRUE)
 
 ## create city data set, 2017
 cityData17 <- filter(fullData, GeographicLevel == "City")
